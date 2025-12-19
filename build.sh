f1e029d8d61cf824f149fc6245f9c0800e56beb1
ybutola <yogibutola@icloud.com>
2025-12-19 13:15:59 -0600

    build: add cffi 2.0.0 and update dependency lock files.

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 poetry lock
 poetry export -f requirements.txt --without-hashes > requirements.txt
 docker build -t capitalreport:v1.0.0 .
-docker compose -f docker-compose.yml up
+# docker compose -f docker-compose.yml up
